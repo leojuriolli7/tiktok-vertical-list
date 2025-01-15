e8b5e84dc5bc03789d9ab9c6330b0bc33bea1865
leojuriolli7 <leojuriolli@gmail.com>
2025-01-15 09:23:02 -0300

    feat: add videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,33 @@ import VerticalScrollList from "./components/VerticalScrollList";
 export default function Home() {
   return (
     <VerticalScrollList>
-      <div className="bg-red-500 h-full">Video 1</div>
-      <div className="bg-blue-500 h-full">Video 2</div>
-      <div className="bg-green-500 h-full">Video 3</div>
+      <div className="bg-red-500 h-full">
+        <video
+          className="h-full w-full object-cover"
+          muted
+          controls={false}
+          autoPlay
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        />
+      </div>
+      <div className="bg-blue-500 h-full">
+        <video
+          className="h-full w-full object-cover"
+          muted
+          controls={false}
+          autoPlay
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        />
+      </div>
+      <div className="bg-green-500 h-full">
+        <video
+          className="h-full w-full object-cover"
+          muted
+          controls={false}
+          autoPlay
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+        />
+      </div>
     </VerticalScrollList>
   );
 }
